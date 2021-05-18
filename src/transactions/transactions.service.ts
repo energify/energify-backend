862000bfa7d8455a75b0887379cad7ecd1a07d30
Vasco Sousa <myemailvasco@gmail.com>
2021-05-18 17:12:24 +0100

    :recycle: Removed db save

--- src/transactions/transactions.service.ts
+++ src/transactions/transactions.service.ts
@@ -207,7 +207,6 @@ export class TransactionsService {
       index++;
       console.log(transactions.length);
     } while (matches.length !== 0);
-
-    await this.transactionsRepository.save(transactions);
+    transactions = [];
   }
 }
